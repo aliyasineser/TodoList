23afc6871a6c522f936159ed5301a898f4e1a2d8
Ali Yasin Eser <aliyasineser@gmail.com>
2023-01-29 21:05:00 +0100

    removed user defaults

--- TodoList/Services/NotesDatabaseFactory.swift
+++ TodoList/Services/NotesDatabaseFactory.swift
@@ -9,17 +9,14 @@ import Foundation
 
 public enum DBTypes {
     case firebase
-    case userDefaults
 }
 
 final class NotesDatabaseFactory {
 
-    func create(type: DBTypes = .userDefaults) -> any NotesDB {
+    func create(type: DBTypes = .firebase) -> any NotesDB {
         switch type {
         case .firebase:
             return NotesFirestoreDB()
-        case .userDefaults:
-            return NotesUserDefaultDB()
         }
     }
 
